258705f6e1ca9200a029077d9ca575ece70d426e
TImmyWong <110480669+twph@users.noreply.github.com>
2023-12-24 20:55:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function playMusic() {
+    var bgMusic = document.getElementById('bgMusic');
+    bgMusic.loop = true; 
+    bgMusic.play();
+  }
+
+function showImage(){
+    var myImage = document.getElementById('marycontainer');
+    var myImage2 = document.getElementById('marycontainer2');
+    var myImage3 = document.getElementById('marycontainer3');
+
+        myImage.style.display = 'flex';
+        myImage2.style.display = 'flex';
+        myImage3.style.display = 'flex';
+
+    
+}
